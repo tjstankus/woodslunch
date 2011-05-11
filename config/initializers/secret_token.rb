e2c06570b42a4bdf2d4cc3754a5f51c9ac7552f2
TJ Stankus <tj@stank.us>
2011-05-11 09:30:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woodslunch::Application.config.secret_token = '53c53edcd3c3572c7c235892bf9ab01254321a516326c8711554d8f99e07ce45acfc1cb512eda39c56602c3d0ef868ddbd330caadb8f9ffa558c4ad34c8dc5b9'
