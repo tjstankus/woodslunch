beb3333a8b0ca22d3e52f9977bf94d6280b91608
TJ Stankus <tj@stank.us>
2011-05-11 23:22:40 -0400

    ooh stylish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def header_link
+    link_content = image_tag('logo.png') + 
+                   content_tag(:span, 'Lunch Ordering', :id => 'heading')
+    link_to(link_content, root_path, :id => 'header_link')
+  end
 end
---
+++ config/compass.rb
@@ -0,0 +1,41 @@
+# This configuration file works with both the Compass command line tool and within Rails.
+# Require any additional compass plugins here.
+project_type = :rails
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+
+# Heroku-specific settings
+# See http://devcenter.heroku.com/articles/using-compass
+project_path = Compass::AppIntegration::Rails.root
+environment  = Compass::AppIntegration::Rails.env
+
+http_path = '/'
+css_dir   = 'tmp/stylesheets'
+sass_dir  = 'app/stylesheets'
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+  :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp")
