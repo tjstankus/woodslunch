f852db646cfcbb966fd55da06faa224ab1d8f99f
TJ Stankus <tj@stank.us>
2011-05-13 23:19:31 -0400

    share sign_in_as method and begin sign out example

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -4,13 +4,13 @@ describe 'sessions' do
 
   let(:user) { FactoryGirl.create(:user) }
 
+  
   it 'sign in' do
-    visit new_user_session_path
-    fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => 'secret'
-    click_button 'Sign in'
-    page.should have_xpath('//div[@id="notice"]', :text => 'Signed in successfully.')
+    sign_in_as(user)
   end
 
-  it 'sign out'
+  it 'sign out' do
+    sign_in_as(user)
+    click_link 'Sign out'
+  end
 end
---
+++ spec/support/session_helpers.rb
@@ -0,0 +1,11 @@
+module SessionHelpers
+
+  def sign_in_as(user)
+    visit new_user_session_path
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => 'secret'
+    click_button 'Sign in'
+    page.should have_xpath('//div[@id="notice"]', :text => 'Signed in successfully.')
+  end
+
+end
