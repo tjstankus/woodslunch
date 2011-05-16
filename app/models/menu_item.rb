13c45cb8a94b13fcc426ed9b7bc4db5a577694e7
TJ Stankus <tj@stank.us>
2011-05-16 16:12:09 -0400

    menu item default price on new record

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,7 +2,15 @@ class MenuItem < ActiveRecord::Base
 
   validates :name, :presence => true
   validates_uniqueness_of :name, :case_sensitive => false
+  validates :price, :presence => true, 
+                    :numericality => { :message => 'must be a number' }
 
   DEFAULT_PRICE = 4.00
+
+  after_initialize :set_price_on_new_record
+
+  def set_price_on_new_record
+    self.price = DEFAULT_PRICE if self.new_record?
+  end
 
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe MenuItem do
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price).with_message(/must be a number/) }
 
   context 'given a saved menu item' do
     let!(:menu_item) { FactoryGirl.create(:menu_item) }
     it { should validate_uniqueness_of(:name).case_insensitive }
+  end
+
+  context 'sets default price attribute on new record' do
+    MenuItem.new.price.should == MenuItem::DEFAULT_PRICE
   end
 end
