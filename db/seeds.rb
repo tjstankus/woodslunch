5a8c0c05abb62ef3b4decf3a000a9395f6da5c31
TJ Stankus <tj@stank.us>
2011-05-30 10:56:02 -0400

    Seeding for menu items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -21,3 +23,50 @@ end
 DayOfWeek::NAMES.each do |day_name|
   DayOfWeek.find_or_create_by_name(day_name)
 end
+
+menu_items_by_day = {
+  'Monday' => [
+    'Hamburger',
+    'Cheeseburger',
+    'Hot Dog',
+    'Veggie Burger',
+    'Entrée Salad',
+    'Cali Sushi Roll',
+    'Veggie Sushi Roll'
+  ],
+  'Tuesday' =>  [
+    'Chicken Fajitas',
+    'Indian Chicken',
+    'Indian Veggie',
+    'Grilled Cheese',
+    'Chile con Queso'
+  ],
+  'Wednesday' => [
+    'Chick-fil-A Sandwich',
+    'Chick-fil-A Nuggets',
+    'Entrée Salad'
+  ],
+  'Thursday' => [
+    'Italian Sub',
+    'Club Sub',
+    'Turkey Sub',
+    'Veggie Sub',
+    'Cheese Burrito'
+  ],
+  'Friday' => [
+    'Cheese Pizza',
+    'Pepperoni Pizza',
+    'Entrée Salad'
+  ]
+}
+
+menu_items_by_day.each do |day_name, item_names|
+  day = DayOfWeek.find_by_name(day_name)
+  item_names.each do |item_name|
+    menu_item = MenuItem.find_or_create_by_name(item_name)
+    unless day.menu_items.include?(menu_item)
+      DailyMenuItem.create!(:day_of_week => day, :menu_item => menu_item)
+    end
+  end
+end
+
