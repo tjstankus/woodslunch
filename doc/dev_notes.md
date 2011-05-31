550efb804e4efc72636884f5cc42298cb06a6b19
TJ Stankus <tj@stank.us>
2011-05-31 01:09:31 -0400

    Persist sign in notice in footer.

---
+++ doc/dev_notes.md
@@ -0,0 +1,14 @@
+Dev Notes
+=========
+
+User login and submit order
+-----------------------------
+
+- User goes to home page
+- User links to sign in page
+- User signs in
+- User is presented with dashboard (home) which shows link to order form for
+  each student, account balance.
+- User clicks link to order for student
+- User is presented with month order view
+- User fills in month order view. Account balance is updated accordingly. 
--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -4,15 +4,34 @@ feature 'home page' do
 
   context 'given a logged in user' do
 
-    it 'displays a logged in message'
+    let(:user) { FactoryGirl.create(:user) }
 
-    it 'displays user account info'
+    before(:each) do
+      sign_in_as(user)
+    end
+
+    it 'displays a logged in message' do
+      page.should have_xpath("//div[@id='notice']", 
+        :text => 'Signed in successfully.')
+    end
+
+    it 'displays user account info' do
+      page.should have_xpath("//span[@id='signed_in_as']", :text => user.email)
+    end
 
   end
 
   context 'for public browsing' do
+
+    before(:each) do
+      # Given I am not logged in
+      # When I go to the home page
+      visit root_path
+    end
     
-    it 'displays the login form'
+    it 'displays a link to the sign in page' do
+      page.should have_xpath("//a[contains(@href, '/users/sign_in')]")  
+    end
 
     it 'displays a link to the account request page'
 
