704cf5df1e76081703ef966d76e769ef0d9f69d1
TJ Stankus <tj@stank.us>
2011-07-01 23:48:45 -0400

    Specs for adding OrderedMenuItem#quantity.

--- spec/models/ordered_menu_item_spec.rb
+++ spec/models/ordered_menu_item_spec.rb
@@ -5,4 +5,12 @@ describe OrderedMenuItem do
   it { should belong_to(:order) }
   it { should validate_presence_of(:menu_item_id) }
   it { should validate_presence_of(:order_id) }
+  it { should validate_presence_of(:quantity) }
+  it { should validate_numericality_of(:quantity) }
+
+  context '::MAX_DISPLAYED_QUANTITY' do
+    it 'is an integer' do |variable|
+      OrderedMenuItem::MAX_DISPLAYED_QUANTITY.should be_an(Integer)
+    end
+  end
 end
