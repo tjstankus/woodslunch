ced5d777d943a4a795981dc0dc1c9f5c32d8d6bb
TJ Stankus <tj@stank.us>
2011-07-08 00:20:34 -0400

    Cleanup of nav styles, js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
 
   $('ul.dropdown li').hover(function() {
 
-      $(this).addClass('hover');
       $('ul:first', this).css('visibility', 'visible');
 
     }, function() {
 
-      $(this).removeClass('hover');
       $('ul:first', this).css('visibility', 'hidden');
-
   });
 });
