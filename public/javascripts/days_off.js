af43717c408701808232fbcf084fb87afffe51f6
TJ Stankus <tj@stank.us>
2011-07-11 09:39:50 -0400

    Format datepicker so that ruby can parse it

--- public/javascripts/days_off.js
+++ public/javascripts/days_off.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $('#day_off_starts_on').datepicker();
-$('#day_off_ends_on').datepicker();
+  $('#day_off_starts_on').datepicker({dateFormat: 'yy-mm-dd'});
+  $('#day_off_ends_on').datepicker({dateFormat: 'yy-mm-dd'});
 });
