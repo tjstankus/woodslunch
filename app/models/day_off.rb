db1378c438c3770ec5e3df10a40b689c7fc7c0c2
TJ Stankus <tj@stank.us>
2011-07-13 21:43:41 -0400

    DayOff date uniqueness validations, query for affected date.

--- app/models/day_off.rb
+++ app/models/day_off.rb
@@ -1,9 +1,13 @@
 class DayOff < ActiveRecord::Base
 
   validates :name, :presence => true
-  validates :starts_on, :presence => true
-  validates :ends_on, :presence => true
+  validates :starts_on, :presence => true, :uniqueness => true
+  validates :ends_on, :presence => true, :uniqueness => true
   validate :start_must_be_on_or_before_end
+
+  def self.for_date(date)
+    where("starts_on >= :date OR ends_on <= :date", {:date => date.to_s}).first
+  end
 
   def start_must_be_on_or_before_end
     unless (starts_on && ends_on) && (starts_on <= ends_on)
--- spec/models/day_off_spec.rb
+++ spec/models/day_off_spec.rb
@@ -5,9 +5,27 @@ describe DayOff do
   it { should validate_presence_of(:starts_on) }
   it { should validate_presence_of(:ends_on) }
 
+  context 'given a saved day off' do
+    let!(:day_off) { Factory(:day_off) }
+    it { should validate_uniqueness_of(:starts_on) }
+    it { should validate_uniqueness_of(:ends_on) }
+  end
+
   it 'validates that starts_on <= ends_on' do
     day_off = Factory.build(:day_off, :starts_on => '2011-09-09', :ends_on => '2011-09-08')
     day_off.should_not be_valid
     day_off.errors[:base].should_not be_empty
+  end
+
+  describe '.for_date' do
+    it 'returns DayOff object for date that is a day off' do
+      day_off = Factory(:day_off)
+      DayOff.for_date(day_off.starts_on).should == day_off
+    end
+
+    it 'returns nil for date that is not a day off' do
+      d = Date.parse('2010-08-24')
+      DayOff.for_date(d).should be_nil
+    end
   end
 end
