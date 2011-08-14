6b4dcbd5f5f5b8a71638baa19bc8821e454abe84
TJ Stankus <tj@stank.us>
2011-08-13 20:50:16 -0400

    Order spec passing, mostly commented out.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe Order do
   it { should have_many(:menu_items).through(:ordered_menu_items) }
   it { should validate_presence_of(:served_on) }
-  it { should validate_presence_of(:starts_on) }
-  it { should validate_presence_of(:ends_on) }
 
 
   # TODO: This may change with STI experiment
