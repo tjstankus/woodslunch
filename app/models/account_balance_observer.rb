e8bd7b77df5cc852a9a0ccf733d4970e5c03e0dd
TJ Stankus <tj@stank.us>
2011-08-14 13:32:05 -0400

    Remove unnecessary code

--- app/models/account_balance_observer.rb
+++ app/models/account_balance_observer.rb
@@ -1,19 +1,6 @@
 class AccountBalanceObserver < ActiveRecord::Observer
   observe OrderedMenuItem
 
-  # def after_create(model)
-  #   account = model.get_account
-  #   account.change_balance_by(model.total)
-  # end
-
-  # def after_update(model)
-  #   if model.total_changed?
-  #     diff = model.total - model.total_was
-  #     account = model.get_account
-  #     account.change_balance_by(model.total)
-  #   end
-  # end
-
   def before_save(model)
     if model.total_changed?
       diff = model.total - model.total_was
