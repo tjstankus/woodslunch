21ac294377ef7d551e8499f996dc6b95e5722ec6
TJ Stankus <tj@stank.us>
2011-09-02 11:35:42 -0400

    Fix JS. There was an issue when another element on the page had the same id as the datepicker text field. Also, only show submit button after a date has been selected.

--- public/javascripts/reports.js
+++ public/javascripts/reports.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  $('input#date').datepicker({dateFormat: 'yy-mm-dd'});
+  $('input#date').datepicker({
+    dateFormat: 'yy-mm-dd',
+    onSelect: function(dateText, inst) {
+      $('form input[type=submit]').show();
+    }
+  });
 });
