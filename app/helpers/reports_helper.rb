32fdee5075f26dfd75281beb4e76d399fc799722
TJ Stankus <tj@stank.us>
2011-09-03 15:32:45 -0400

    Reports UI

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -9,4 +9,26 @@ module ReportsHelper
       end
     end
   end
+
+  def reports_for_grade(grade)
+    return unless @orders[grade]
+    ''.tap do |html|
+      @orders[grade].each do |order|
+        underscored_klass = order.class.to_s.underscore
+        html << content_tag(:tr,
+                            :id => underscored_klass + "_#{order.id}",
+                            :class => cycle('odd', 'even') + " #{underscored_klass}") do
+          content_tag(:td, order.last_name, :class => 'last_name') +
+          content_tag(:td, order.first_name, :class => 'first_name') +
+          content_tag(:td, order.grade, :class => 'grade') +
+          ''.tap do |menu_items_html|
+            @menu_items.each do |menu_item|
+              menu_items_html << content_tag(:td, display_menu_item_for_order(order, menu_item), :class => 'menu_item')
+            end
+          end.html_safe
+        end
+      end
+    end.html_safe
+  end
+
 end
