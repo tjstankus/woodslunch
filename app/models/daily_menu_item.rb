5da3655458362dd13ca19b13be256729eb269d8f
TJ Stankus <tj@stank.us>
2012-03-20 23:51:34 -0400

    Added availability filter method.

--- app/models/daily_menu_item.rb
+++ app/models/daily_menu_item.rb
@@ -1,7 +1,12 @@
 class DailyMenuItem < ActiveRecord::Base
   belongs_to :day_of_week
   belongs_to :menu_item
+  has_many :availabilities, :class_name => 'DailyMenuItemAvailability'
 
   validates :day_of_week_id, :presence => true
   validates :menu_item_id, :presence => true
+
+  def available_on_date?(date)
+    availabilities.empty? ? true : availabilities.first.available_on_date?(date)
+  end
 end
--- spec/models/daily_menu_item_spec.rb
+++ spec/models/daily_menu_item_spec.rb
@@ -3,4 +3,36 @@ require 'spec_helper'
 describe DailyMenuItem do
   it { should validate_presence_of(:menu_item_id) }
   it { should validate_presence_of(:day_of_week_id) }
+  it { should have_many(:availabilities).class_name('DailyMenuItemAvailability') }
+
+  it 'has no associated availabilities by default' do
+    dmi = Factory.build(:daily_menu_item)
+    dmi.availabilities.should be_empty
+  end
+
+  describe '#available_on_date?' do
+
+    context 'given no associated availablilites' do
+
+      let(:dmi) { Factory.build(:daily_menu_item) }
+
+      it 'returns true' do
+        dmi.available_on_date?(Date.today).should be_true
+      end
+
+    end
+
+    context 'given associated availabilities' do
+
+      let!(:dmia) { Factory(:daily_menu_item_availability) }
+      let!(:dmi) { dmia.daily_menu_item }
+
+      it 'forwards to the first associated availability' do
+        date = Date.today
+        DailyMenuItemAvailability.any_instance.should_receive(:available_on_date?).with(date)
+        dmi.available_on_date?(date)
+      end
+    end
+
+  end
 end
