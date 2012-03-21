d26f0f2996f0b4ce4933e0c5322bec20b4ed0771
TJ Stankus <tj@stank.us>
2012-03-21 19:46:24 -0400

    Open up orders to end of school year.

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,2 +1,2 @@
 configatron.orders_first_available_on = Date.parse('2011-09-06')
-configatron.orders_last_available_on = Date.parse('2012-03-31')
+configatron.orders_last_available_on = Date.parse('2012-06-08')
