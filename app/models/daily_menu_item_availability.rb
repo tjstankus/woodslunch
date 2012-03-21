eabdf8ab32a3a034c5c0c796eb8caa9aac02c9ef
TJ Stankus <tj@stank.us>
2012-03-20 22:08:56 -0400

    Basic functionality for DailyMenuItemAvailability in place.

--- app/models/daily_menu_item_availability.rb
+++ app/models/daily_menu_item_availability.rb
@@ -1,6 +1,20 @@
+# NOTE: This class was designed to do more than it currently does. I imagine
+#       that down the road there may be a need/request that would lead to a
+#       temporary availability, so I added an ends_on attribute that we
+#       currently expect to be nil. I can even foresee a request for multiple
+#       temporary availabilities, in which case we'd need to guard against
+#       overlapping timeframes and other manner of messiness. In fact, at that
+#       point, it may be time to back up and think about refactoring menu
+#       scheduling altogether.
+
 class DailyMenuItemAvailability < ActiveRecord::Base
   belongs_to :daily_menu_item
   validates :daily_menu_item_id, :presence => true
   validates :starts_on, :presence => true
   validates :available, :presence => true
+
+  def available_on_date?(date)
+    raise NotImplementedError unless ends_on.blank?
+    date >= starts_on ? available : !available
+  end
 end
--- spec/requests/daily_menu_item_availability_spec.rb
+++ spec/requests/daily_menu_item_availability_spec.rb
@@ -5,11 +5,16 @@ describe 'Daily menu item availability' do
   context 'given a daily_menu_item' do
 
     before(:each) do
+      # TODO: Refactor this setup code. It's too much, which smells.
       time = Time.now
       @year = time.year
       @month = time.month
+      configatron.orders_first_available_on = time.to_date - 1.month
+      configatron.orders_last_available_on = time.to_date + 3.months
       @daily_menu_item = Factory(:daily_menu_item)
       @student = create_student
+      @user = @student.account.users.first
+      sign_in_as @user
     end
 
     context 'that is not available until next month' do
@@ -19,13 +24,11 @@ describe 'Daily menu item availability' do
       end
 
       it "does not appear on this month's order form" do
-        pending 'sign in'
         visit student_orders_path(@student, :year => @year, :month => @month)
         page.should_not have_content @daily_menu_item.menu_item.name
       end
 
       it "appears on next month's order form" do
-        pending 'sign in'
         visit student_orders_path(@student, :year => @year, :month => @month + 1)
         page.should have_content @daily_menu_item.menu_item.name
       end
