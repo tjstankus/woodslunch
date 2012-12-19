27fbaf6971a12cb7a2325a0cafb0b561f7fe4ef3
TJ Stankus <tj@stank.us>
2012-12-18 23:28:47 -0500

    Improve date math for displaying month order links

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,11 +7,13 @@ module OrdersHelper
   def available_order_months_first_dates
     first_date = first_available_order_date.beginning_of_month
     last_date = configatron.orders_last_available_on.beginning_of_month
-    dates = Array.new((first_date.month..last_date.month).to_a.size)
-    dates.size.times do |i|
-      dates[i] = first_date + i.months
+    date = first_date
+    [].tap do |dates|
+      until date > last_date
+        dates << date
+        date = date + 1.month
+      end
     end
-    dates
   end
 
 end
