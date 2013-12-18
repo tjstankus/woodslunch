360c90a2ae8e35dbd158e5fadc8535d2d1eefc4e
TJ Stankus <tj@stank.us>
2013-12-18 11:30:25 -0500

    Open orders through March 2014

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,2 +1,2 @@
 configatron.orders_first_available_on = Date.parse('2013-09-03')
-configatron.orders_last_available_on = Date.parse('2013-12-31')
+configatron.orders_last_available_on = Date.parse('2014-03-31')
